d191f8ba4bfaf4563207dc88fd5d8bea2820e88f
PichuDestroyer <elvincheung0321@gmail.com>
2025-03-31 20:45:42 +0800

    update for deployement

--- api/index.py
+++ api/index.py
@@ -4,9 +4,5 @@ from django.core.wsgi import get_wsgi_application
 # Set the settings module for Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
 
-# Create the WSGI application
-application = get_wsgi_application()
-
-def handler(environ, start_response):
-    response = application(environ, start_response)
-    return response
+# Expose the WSGI application as `app`
+app = get_wsgi_application()
