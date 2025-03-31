41fa7523b4e21b93f33fce42197608253648e281
PichuDestroyer <elvincheung0321@gmail.com>
2025-03-31 20:47:55 +0800

    update for deployement

--- api/index.py
+++ api/index.py
@@ -1,8 +1,5 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-# Set the settings module for Django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
-
-# Expose the WSGI application as `app`
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nba.settings')  # Replace 'nba' with your project name
 app = get_wsgi_application()
